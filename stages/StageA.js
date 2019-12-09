6b8e60e162bdf73cfdadc45dd37e602af538f1c7
Hanako-Ikezawa <33704003+Hanako-Ikezawa@users.noreply.github.com>
2019-12-09 16:37:39 -0500

    Update StageA.js

--- stages/StageA.js
+++ stages/StageA.js
@@ -7,7 +7,7 @@ var stageA = {
 
         //THESE VARIABLES NEED TO BE INIT FIRST FOR SOME REASON/
         //I THINK IT'S CAUSE OF WEBPAGE LOAD TIME
-        this.bgm = 'My happy song Nicholas w lott.mp3';
+        this.bgm = '.sounds/My happy song Nicholas w lott.mp3';
 
 
         this.omega = omegaOn;
