c55c079f9ba803172616012e52bc3d8794bfbd63
Leonardo Ascenzi <lascenzi@colgate.edu>
2019-12-11 16:31:11 -0500

    added stage select file

--- game.js
+++ game.js
@@ -269,47 +269,6 @@ function stageSelectLoop(){
 
 }
 
-//builds the colgate map with locations to play on
-function buildStageSelect(){
-  mapScene = new THREE.Scene();
-  sunsetLights(mapScene);
-
-
-  var planeGeom = new THREE.BoxGeometry(10,10,10,1,1,1);
-  var planeMat = new THREE.MeshPhongMaterial({color: 0xffffff});
-  var planeMesh = new THREE.Mesh(planeGeom, planeMat);
-  planeMesh.userData = {stageData:stageA, omega: false};
-  mapScene.add(planeMesh);
-  selectableStages.push(planeMesh);
-
-  var planeGeom2 = new THREE.BoxGeometry(10,10,10,1,1,1);
-  var planeMat2 = new THREE.MeshPhongMaterial({color: 0x00ff00});
-  var planeMesh2 = new THREE.Mesh(planeGeom2, planeMat2);
-  planeMesh2.userData = {stageData:stageA, omega: true};
-  mapScene.add(planeMesh2);
-  planeMesh2.position.y += 10;
-  selectableStages.push(planeMesh2);
-
-  var planeGeom3 = new THREE.BoxGeometry(10,10,10,1,1,1);
-  var planeMat3 = new THREE.MeshPhongMaterial({color: 0x0000ff});
-  var planeMesh3 = new THREE.Mesh(planeGeom3, planeMat3);
-  planeMesh3.userData = {stageData:stageB, omega: false};
-  mapScene.add(planeMesh3);
-  planeMesh3.position.x += 20;
-  selectableStages.push(planeMesh3);
-
-  var planeGeom4 = new THREE.BoxGeometry(10,10,10,1,1,1);
-  var planeMat4 = new THREE.MeshPhongMaterial({color: 0xff00ff});
-  var planeMesh4 = new THREE.Mesh(planeGeom4, planeMat4);
-  planeMesh4.userData = {stageData:stageB, omega: true};
-  mapScene.add(planeMesh4);
-  planeMesh4.position.x += 20;
-  planeMesh4.position.y += 10;
-  selectableStages.push(planeMesh4);
-
-  camera.lookAt(0,0,0);
-}
-
 //creates the stage and calls the main loop
 function initializeWorld(){
     contols = undefined;
