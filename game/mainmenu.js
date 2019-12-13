a32443c5d4611d66fb299f887870c0a7ac34ae29
Leonardo Ascenzi <lascenzi@colgate.edu>
2019-12-13 14:23:15 -0500

    changed menu texture type anbd removed lights

--- game/mainmenu.js
+++ game/mainmenu.js
@@ -13,11 +13,10 @@ function buildTitleScreen(){
   titleScene = new THREE.Scene();
   camera.position.set(0,0,900);
   camera.lookAt(0,0,0);
-  sunsetLights(titleScene);
 
   // //BACKGROUND of map
   var titleGeom = new THREE.BoxGeometry(WIDTH,HEIGHT,1,1,1,1);
-  var titleMat  = new THREE.MeshPhongMaterial({map: new THREE.TextureLoader().load('images/menu/titlescreen.png')});
+  var titleMat  = new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('images/menu/titlescreen.png')});
   var titleMesh = new THREE.Mesh(titleGeom, titleMat);
   titleScene.add(titleMesh);
 
